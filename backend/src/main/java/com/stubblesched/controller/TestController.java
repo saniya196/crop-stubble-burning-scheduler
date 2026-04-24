bd1cbac370a2c9db68b989ca34a117b1dfc7b807
Saniya <saniyaasreen196@gmail.com>
2026-04-25 00:58:56 +0530

    add cors

--- backend/src/main/java/com/stubblesched/controller/TestController.java
+++ backend/src/main/java/com/stubblesched/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.stubblesched.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @GetMapping("/api/test")
